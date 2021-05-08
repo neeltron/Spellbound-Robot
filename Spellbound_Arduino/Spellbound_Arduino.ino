ee3b3547d7ed17cadc96ddf6d10afa6ccbd03e84
unknown <500075823@stu.upes.ac.in>
2021-05-08 17:59:13 +0530

    Added code for motor shield

---
+++ Spellbound_Arduino/Spellbound_Arduino.ino
@@ -0,0 +1,19 @@
+#include <AFMotor.h>
+AF_DCMotor motor1(3);
+AF_DCMotor motor2(4);
+AF_DCMotor motor3(1);
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(BACKWARD);
+
+}
